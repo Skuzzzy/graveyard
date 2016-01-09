03e0e91c05282c592c9723b336d9419ee5ac1a7c
Daniel Baird <JamesDanielBaird@gmail.com>
2016-01-08 23:50:28 -0700

    update u

--- u.py
+++ u.py
@@ -11,3 +11,11 @@ def c():
     Alias for clear
     """
     clear()
+
+def pretty_dir(obj):
+    print ("\n".join(dir(obj)))
+
+def pdir(obj):
+    pretty_dir(obj)
+def doc(obj):
+    print (obj.__doc__)
